e6fc428c1d43f7649369de33a4cf0ba2af83833e
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-12-14 22:38:24 -0500

    Add IConDP3Encoder

--- 3D-Diffusion-Policy/diffusion_policy_3d/model/vision/pointnet_extractor.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/model/vision/pointnet_extractor.py
@@ -6,6 +6,8 @@ import copy
 
 from typing import Optional, Dict, Tuple, Union, List, Type
 from termcolor import cprint
+from diffusion_policy_3d.common.pc_util import sample_and_group
+from diffusion_policy_3d.common.loss_util import info_nce_loss
 
 
 def create_mlp(
@@ -198,6 +200,140 @@ class PointNetEncoderXYZ(nn.Module):
         """
         self.input_pointcloud = input[0].detach()
 
+
+class IConPointNetEncoderXYZ(nn.Module):
+    """Encoder for Pointcloud
+    """
+
+    def __init__(self,
+                 in_channels: int=3,
+                 out_channels: int=1024,
+                 use_layernorm: bool=False,
+                 final_norm: str='none',
+                 use_projection: bool=True,
+                 icon_cfg: Dict=None
+                 ):
+        """_summary_
+
+        Args:
+            in_channels (int): feature size of input (3 or 6)
+            input_transform (bool, optional): whether to use transformation for coordinates. Defaults to True.
+            feature_transform (bool, optional): whether to use transformation for features. Defaults to True.
+            is_seg (bool, optional): for segmentation or classification. Defaults to False.
+        """
+        super().__init__()
+        block_channel = [64, 128, 256]
+        cprint("[PointNetEncoderXYZ] use_layernorm: {}".format(use_layernorm), 'cyan')
+        cprint("[PointNetEncoderXYZ] use_final_norm: {}".format(final_norm), 'cyan')
+        
+        assert in_channels == 3, cprint(f"PointNetEncoderXYZ only supports 3 channels, but got {in_channels}", "red")
+       
+        self.mlp = nn.Sequential(
+            nn.Linear(in_channels, block_channel[0]),
+            nn.LayerNorm(block_channel[0]) if use_layernorm else nn.Identity(),
+            nn.ReLU(),
+            nn.Linear(block_channel[0], block_channel[1]),
+            nn.LayerNorm(block_channel[1]) if use_layernorm else nn.Identity(),
+            nn.ReLU(),
+            nn.Linear(block_channel[1], block_channel[2]),
+            nn.LayerNorm(block_channel[2]) if use_layernorm else nn.Identity(),
+            nn.ReLU(),
+        )
+        
+        
+        if final_norm == 'layernorm':
+            self.final_projection = nn.Sequential(
+                nn.Linear(block_channel[-1], out_channels),
+                nn.LayerNorm(out_channels)
+            )
+        elif final_norm == 'none':
+            self.final_projection = nn.Linear(block_channel[-1], out_channels)
+        else:
+            raise NotImplementedError(f"final_norm: {final_norm}")
+
+        self.use_projection = use_projection
+        if not use_projection:
+            self.final_projection = nn.Identity()
+            cprint("[PointNetEncoderXYZ] not use projection", "yellow")
+            
+        VIS_WITH_GRAD_CAM = False
+        if VIS_WITH_GRAD_CAM:
+            self.gradient = None
+            self.feature = None
+            self.input_pointcloud = None
+            self.mlp[0].register_forward_hook(self.save_input)
+            self.mlp[6].register_forward_hook(self.save_feature)
+            self.mlp[6].register_backward_hook(self.save_gradient)
+
+        # Inter-point contrast
+        self.feat_agg_method = icon_cfg['feat_agg_method']  # Method to aggregate features into query ("mean" or "max")
+        self.sample_method = icon_cfg['sample_method']
+        self.sample_ratio = icon_cfg['sample_ratio']
+        self.group_method = icon_cfg['group_method']
+        self.num_neighbors = icon_cfg['num_neighbors']
+        self.temperature = icon_cfg['temperature']
+
+    def forward_loss(self, xyz, feature, mask):
+        """
+        X: (B, N, 3)
+        feature: (B, N, C)
+        mask: (B, N)
+        """
+        eps = 1e-8
+        if self.feat_agg_method == "mean":
+            query = (feature * mask[..., None]).sum(dim=1) / (mask.sum(dim=1)[:, None] + eps)
+        elif self.feat_agg_method == "max":
+            query = feature.masked_fill(~mask[..., None], float('-inf')).max(dim=1)[0]
+
+        batch_idx, pos_key, neg_key = sample_and_group(
+            pc_xyz=xyz,
+            pc_feat=feature,
+            pc_mask=mask,
+            sample_method=self.sample_method,
+            sample_ratio=self.sample_ratio,
+            group_method=self.group_method,
+            num_neighbors=self.num_neighbors
+        )
+        query = query[batch_idx]
+        loss = info_nce_loss(
+            query=query,
+            pos_key=pos_key,
+            neg_key=neg_key,
+            batch_idx=batch_idx,
+            temp=self.temperature,
+            reduction='mean'
+        )
+        return loss
+         
+    def forward(self, xyz, mask=None):
+        feature = self.mlp(xyz)
+        feature_agg = torch.max(feature, 1)[0]
+        feature_agg = self.final_projection(feature_agg)
+        if mask is not None:
+            loss = self.forward_loss(xyz, feature, mask)
+            return feature_agg, loss
+        return feature_agg
+    
+    def save_gradient(self, module, grad_input, grad_output):
+        """
+        for grad-cam
+        """
+        self.gradient = grad_output[0]
+
+    def save_feature(self, module, input, output):
+        """
+        for grad-cam
+        """
+        if isinstance(output, tuple):
+            self.feature = output[0].detach()
+        else:
+            self.feature = output.detach()
+    
+    def save_input(self, module, input, output):
+        """
+        for grad-cam
+        """
+        self.input_pointcloud = input[0].detach()
     
 
 
@@ -258,6 +394,81 @@ class DP3Encoder(nn.Module):
         state_feat = self.state_mlp(state)  # B * 64
         final_feat = torch.cat([pn_feat, state_feat], dim=-1)
         return final_feat
+
+
+    def output_shape(self):
+        return self.n_output_channels
+    
+
+class IConDP3Encoder(nn.Module):
+    def __init__(self, 
+                 observation_space: Dict, 
+                 img_crop_shape=None,
+                 out_channel=256,
+                 state_mlp_size=(64, 64), state_mlp_activation_fn=nn.ReLU,
+                 pointcloud_encoder_cfg=None,
+                 use_pc_color=False,
+                 pointnet_type='pointnet',
+                 ):
+        super().__init__()
+        self.state_key = ['robot0_eef_pos', 'robot0_eef_quat', 'robot0_gripper_qpos']
+        self.point_cloud_key = [key for key in observation_space.keys() if key.endswith("pc")]
+        self.point_cloud_shape = observation_space[self.point_cloud_key[0]]
+        self.state_shape = [sum(observation_space[key][0] for key in self.state_key)]
+        self.pc_mask_key = [key for key in observation_space.keys() if key.endswith("pc_mask")]
+        
+        cprint(f"[DP3Encoder] point cloud shape: {self.point_cloud_shape}", "yellow")
+        cprint(f"[DP3Encoder] state shape: {self.state_shape}", "yellow")
+
+        self.use_pc_color = use_pc_color
+        self.pointnet_type = pointnet_type
+        backbone = None
+        if pointnet_type == "pointnet":
+            if use_pc_color:
+                pointcloud_encoder_cfg.in_channels = 6
+                backbone = PointNetEncoderXYZRGB(**pointcloud_encoder_cfg)
+            else:
+                pointcloud_encoder_cfg.in_channels = 3
+                backbone = PointNetEncoderXYZ(**pointcloud_encoder_cfg)
+        else:
+            raise NotImplementedError(f"pointnet_type: {pointnet_type}")
+        self.extractor = nn.ModuleDict({key: copy.deepcopy(backbone) for key in self.point_cloud_key})
+        self.n_output_channels = out_channel * len(self.point_cloud_key)
+
+        if len(state_mlp_size) == 0:
+            raise RuntimeError(f"State mlp size is empty")
+        elif len(state_mlp_size) == 1:
+            net_arch = []
+        else:
+            net_arch = state_mlp_size[:-1]
+        output_dim = state_mlp_size[-1]
+
+        self.n_output_channels  += output_dim
+        self.state_mlp = nn.Sequential(*create_mlp(self.state_shape[0], output_dim, net_arch, state_mlp_activation_fn))
+
+        cprint(f"[DP3Encoder] output dim: {self.n_output_channels}", "red")
+
+
+    def forward(self, observations: Dict) -> torch.Tensor:
+        state = torch.cat([observations[key] for key in self.state_key], dim=-1)
+        state_feat = self.state_mlp(state)  # B * 64
+        if self.pc_mask_key:
+            pn_feat = contrast_loss = []
+            for key in self.point_cloud_key:
+                feat, loss = self.extractor[key](observations[key], observations[f'{key}_mask'])
+                pn_feat.append(feat)
+                contrast_loss.append(loss)
+            pn_feat = torch.cat(pn_feat, dim=-1)
+            contrast_loss = torch.stack(contrast_loss).mean()
+            final_feat = torch.cat([pn_feat, state_feat], dim=-1)
+            return final_feat, contrast_loss
+        else:
+            pn_feat = torch.cat([
+                self.extractor[key](observations[key]) for key in self.point_cloud_key
+            ], dim=-1)    
+        
+            final_feat = torch.cat([pn_feat, state_feat], dim=-1)
+            return final_feat
 
 
     def output_shape(self):
