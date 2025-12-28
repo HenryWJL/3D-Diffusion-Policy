f2728fd0d355e4cd47e79ef5c42df00db8b3f952
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-12-28 09:01:06 -0500

    Fix key error

--- 3D-Diffusion-Policy/diffusion_policy_3d/model/vision/pointnet_extractor.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/model/vision/pointnet_extractor.py
@@ -357,7 +357,7 @@ class DP3Encoder(nn.Module):
                  pointnet_type='pointnet',
                  ):
         super().__init__()
-        self.state_key = ['robot0_eef_pos', 'robot0_eef_quat', 'robot0_gripper_qpos']
+        self.state_key = [key for key in observation_space.keys() if key.startswith("robot")]
         self.point_cloud_key = [key for key in observation_space.keys() if key.endswith("pc")]
         
         self.point_cloud_shape = observation_space[self.point_cloud_key[0]]
