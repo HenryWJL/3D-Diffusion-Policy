fed9e3fe084882a6d4c726a37a829ce317f59161
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-12-28 08:51:19 -0500

    Add eval.sh

--- 3D-Diffusion-Policy/eval.sh
+++ 3D-Diffusion-Policy/eval.sh
@@ -1,7 +1,7 @@
 policy=${1}
 task=${2}
 
-# python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/1200.pth
-# python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/1800.pth
-# python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/2400.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/1200.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/1800.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/2400.pth
 python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/3000.pth
