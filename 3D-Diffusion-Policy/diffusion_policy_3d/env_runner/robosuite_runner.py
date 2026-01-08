c5608d0aae8267efe4442d9c4682d6d9f38190c0
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-01-07 21:00:30 -0500

    Add SVGD refinement

--- 3D-Diffusion-Policy/diffusion_policy_3d/env_runner/robosuite_runner.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/env_runner/robosuite_runner.py
@@ -10,6 +10,7 @@ from diffusion_policy_3d.policy.base_policy import BasePolicy
 from diffusion_policy_3d.common.pytorch_util import dict_apply
 from diffusion_policy_3d.env_runner.base_runner import BaseRunner
 import diffusion_policy_3d.common.logger_util as logger_util
+from diffusion_policy_3d.common.svgd import ActionSampler, svgd_gradient, svgd_update
 from termcolor import cprint
 
 
@@ -150,6 +151,149 @@ from termcolor import cprint
 
 
 
+# class RobosuiteRunner(BaseRunner):
+
+#     def __init__(
+#         self,
+#         output_dir,
+#         shape_meta,
+#         eval_episodes=20,
+#         max_steps=200,
+#         n_obs_steps=8,
+#         n_action_steps=8,
+#         abs_action=True,
+#         fps=10,
+#         crf=22,
+#         render_size=84,
+#         tqdm_interval_sec=5.0,
+#         task_name=None,
+#         bounding_boxes=dict(),
+#     ):
+#         super().__init__(output_dir)
+#         self.shape_meta = shape_meta
+#         self.task_name = task_name
+
+#         steps_per_render = max(10 // fps, 1)
+
+#         def env_fn():
+#             return MultiStepWrapper(
+#                 SimpleVideoRecordingWrapper(
+#                     env=RobosuiteEnv(
+#                         env_name=task_name,
+#                         robots="Panda",
+#                         camera_names=list(bounding_boxes.keys()),
+#                         bounding_boxes=bounding_boxes,
+#                         delta_action=not abs_action,
+#                         render_image_size=(render_size, render_size)
+#                     ),
+#                     steps_per_render=steps_per_render
+#                 ),
+#                 n_obs_steps=n_obs_steps,
+#                 n_action_steps=n_action_steps,
+#                 max_episode_steps=max_steps,
+#                 reward_agg_method='sum',
+#             )
+
+#         self.eval_episodes = eval_episodes
+#         self.env = env_fn()
+
+#         self.fps = fps
+#         self.crf = crf
+#         self.n_obs_steps = n_obs_steps
+#         self.n_action_steps = n_action_steps
+#         self.max_steps = max_steps
+#         self.tqdm_interval_sec = tqdm_interval_sec
+
+#         self.logger_util_test = logger_util.LargestKRecorder(K=3)
+#         self.logger_util_test10 = logger_util.LargestKRecorder(K=5)
+
+#     def run(self, policy: BasePolicy):
+#         device = policy.device
+#         test_start_seed = 10000
+
+#         all_goal_achieved = []
+#         all_success_rates = []
+#         videos = []
+
+#         action_sampler = ActionSampler(
+#             policy,
+#             16,
+#             self.n_obs_steps,
+#             self.n_action_steps,
+#             self.shape_meta['action']['shape'][0],
+#             self.max_steps,
+#             step_size=1e-2
+#         )
+        
+#         for episode_idx in tqdm.tqdm(range(self.eval_episodes), desc=f"Eval in Robosuite {self.task_name} Pointcloud Env",
+#                                      leave=False, mininterval=self.tqdm_interval_sec):
+                
+#             # start rollout
+#             self.env.env.env.seed(test_start_seed + episode_idx)
+#             obs = self.env.reset()
+#             action_sampler.reset()
+
+#             done = False
+#             num_goal_achieved = 0
+#             actual_step_count = 0
+
+#             while not done:
+#                 # create obs dict
+#                 np_obs_dict = {key: obs[key] for key in self.shape_meta['obs'].keys() if not key.endswith('pc_mask')}
+#                 # device transfer
+#                 obs_dict = dict_apply(np_obs_dict,
+#                                       lambda x: torch.from_numpy(x).unsqueeze(0).to(
+#                                           device=device))
+#                 action = action_sampler.update(obs_dict)
+#                 action = action.detach().to('cpu').unsqueeze(0).numpy()
+#                 # # step env
+#                 obs, reward, done, info = self.env.step(action)
+#                 # all_goal_achieved.append(info['goal_achieved'])
+#                 num_goal_achieved += np.sum(info['is_success'])
+#                 done = np.all(done)
+#                 actual_step_count += 1
+
+#             all_success_rates.append(np.sum(info['is_success']))
+#             all_goal_achieved.append(num_goal_achieved)
+#             videos.append(self.env.env.get_video())
+
+#         # log
+#         log_data = dict()
+        
+
+#         log_data['mean_n_goal_achieved'] = np.mean(all_goal_achieved)
+#         log_data['mean_success_rates'] = np.mean(all_success_rates)
+
+#         log_data['test_mean_score'] = np.mean(all_success_rates)
+
+#         cprint(f"test_mean_score: {np.mean(all_success_rates)}", 'green')
+
+#         self.logger_util_test.record(np.mean(all_success_rates))
+#         self.logger_util_test10.record(np.mean(all_success_rates))
+#         log_data['SR_test_L3'] = self.logger_util_test.average_of_largest_K()
+#         log_data['SR_test_L5'] = self.logger_util_test10.average_of_largest_K()
+
+#         # videos = env.env.get_video()
+#         # if len(videos.shape) == 5:
+#         #     videos = videos[:, 0]  # select first frame
+#         # videos_wandb = wandb.Video(videos, fps=self.fps, format="mp4")
+#         # log_data[f'sim_video_eval'] = videos_wandb
+        
+#         # Save videos
+#         import imageio
+#         videos = np.transpose(np.concatenate(videos), (0, 2, 3, 1))  # -> (T, H, W, C)
+#         imageio.mimwrite("rollout.mp4", videos, fps=30, codec='libx264')
+
+#         # clear out video buffer
+#         _ = self.env.reset()
+#         # # clear memory
+#         # videos = None
+#         # del env
+
+#         return log_data
+
+
+
 class RobosuiteRunner(BaseRunner):
 
     def __init__(
@@ -175,16 +319,22 @@ class RobosuiteRunner(BaseRunner):
         steps_per_render = max(10 // fps, 1)
 
         def env_fn():
-            return SimpleVideoRecordingWrapper(
-                env=RobosuiteEnv(
-                    env_name=task_name,
-                    robots="Panda",
-                    camera_names=list(bounding_boxes.keys()),
-                    bounding_boxes=bounding_boxes,
-                    delta_action=not abs_action,
-                    render_image_size=(render_size, render_size)
+            return MultiStepWrapper(
+                SimpleVideoRecordingWrapper(
+                    env=RobosuiteEnv(
+                        env_name=task_name,
+                        robots="Panda",
+                        camera_names=list(bounding_boxes.keys()),
+                        bounding_boxes=bounding_boxes,
+                        delta_action=not abs_action,
+                        render_image_size=(render_size, render_size)
+                    ),
+                    steps_per_render=steps_per_render
                 ),
-                steps_per_render=steps_per_render
+                n_obs_steps=n_obs_steps,
+                n_action_steps=n_action_steps,
+                max_episode_steps=max_steps,
+                reward_agg_method='sum',
             )
 
         self.eval_episodes = eval_episodes
@@ -201,12 +351,14 @@ class RobosuiteRunner(BaseRunner):
         self.logger_util_test10 = logger_util.LargestKRecorder(K=5)
 
     def run(self, policy: BasePolicy):
+        policy.eval()
         device = policy.device
         test_start_seed = 10000
 
         all_goal_achieved = []
         all_success_rates = []
         videos = []
+        K = 10
         
         for episode_idx in tqdm.tqdm(range(self.eval_episodes), desc=f"Eval in Robosuite {self.task_name} Pointcloud Env",
                                      leave=False, mininterval=self.tqdm_interval_sec):
@@ -214,36 +366,38 @@ class RobosuiteRunner(BaseRunner):
             # start rollout
             self.env.env.env.seed(test_start_seed + episode_idx)
             obs = self.env.reset()
-            policy.reset()
 
             done = False
             num_goal_achieved = 0
             actual_step_count = 0
-
-            T = self.max_steps
-            H = self.n_action_steps
-            D = int(self.shape_meta['action']['shape'][0])
-            all_time_actions = np.zeros((T, T + H, D), dtype=np.float32)
-            all_time_scores = np.zeros_like(all_time_actions)
 
             while not done:
                 # create obs dict
                 np_obs_dict = {key: obs[key] for key in self.shape_meta['obs'].keys() if not key.endswith('pc_mask')}
                 # device transfer
                 obs_dict = dict_apply(np_obs_dict,
-                                      lambda x: torch.from_numpy(x).unsqueeze(0).to(
+                                      lambda x: torch.from_numpy(x).unsqueeze(0).expand(K, *x.shape).to(
                                           device=device))
-                # run policy
+                # Predict K samples
                 with torch.no_grad():
-                    action_pred = policy.predict_action(obs_dict)['action']
-                    score = policy.compute_score(action_pred)
-                # device_transfer
-                np_action = dict_apply(action,
-                                            lambda x: x.detach().to('cpu').numpy())
-                action = np_action_dict['action'].squeeze(0)
-                # step env
+                    result = policy.predict_action(obs_dict)
+                    action_preds = result['action_pred'].float()
+                    # actions = result['action'].float()
+                    # score_preds = policy.compute_score(action_preds, obs_dict).float()
+                    # start = self.n_obs_steps - 1
+                    # end = start + self.n_action_steps
+                    # scores = score_preds[:, start: end]
+                    # grads = svgd_gradient(actions, scores)
+                    # actions = actions + 0.01 * grads
+                    action_preds = svgd_update(action_preds, obs_dict, policy)
+                    start = self.n_obs_steps - 1
+                    end = start + self.n_action_steps
+                    actions = action_preds[:, start: end]
+                    action = actions.mean(dim=0)
+                    action = action.detach().to('cpu').numpy()
+                # # step env
                 obs, reward, done, info = self.env.step(action)
-                # all_goal_achieved.append(info['goal_achieved']
+                # all_goal_achieved.append(info['goal_achieved'])
                 num_goal_achieved += np.sum(info['is_success'])
                 done = np.all(done)
                 actual_step_count += 1
