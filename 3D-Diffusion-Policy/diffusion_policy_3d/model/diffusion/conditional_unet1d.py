eaecb157e51ffe56708464ca3ea0d8f056d1bf30
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-01-27 16:46:26 -0500

    Add index conditioning

--- 3D-Diffusion-Policy/diffusion_policy_3d/model/diffusion/conditional_unet1d.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/model/diffusion/conditional_unet1d.py
@@ -146,12 +146,213 @@ class ConditionalResidualBlock1D(nn.Module):
         return out
 
 
+# class ConditionalUnet1D(nn.Module):
+#     def __init__(self, 
+#         input_dim,
+#         local_cond_dim=None,
+#         global_cond_dim=None,
+#         diffusion_step_embed_dim=256,
+#         down_dims=[256,512,1024],
+#         kernel_size=3,
+#         n_groups=8,
+#         condition_type='film',
+#         use_down_condition=True,
+#         use_mid_condition=True,
+#         use_up_condition=True,
+#         ):
+#         super().__init__()
+#         self.condition_type = condition_type
+        
+#         self.use_down_condition = use_down_condition
+#         self.use_mid_condition = use_mid_condition
+#         self.use_up_condition = use_up_condition
+        
+#         all_dims = [input_dim] + list(down_dims)
+#         start_dim = down_dims[0]
+
+#         dsed = diffusion_step_embed_dim
+#         diffusion_step_encoder = nn.Sequential(
+#             SinusoidalPosEmb(dsed),
+#             nn.Linear(dsed, dsed * 4),
+#             nn.Mish(),
+#             nn.Linear(dsed * 4, dsed),
+#         )
+#         cond_dim = dsed
+#         if global_cond_dim is not None:
+#             cond_dim += global_cond_dim
+
+#         in_out = list(zip(all_dims[:-1], all_dims[1:]))
+
+#         local_cond_encoder = None
+#         if local_cond_dim is not None:
+#             _, dim_out = in_out[0]
+#             dim_in = local_cond_dim
+#             local_cond_encoder = nn.ModuleList([
+#                 # down encoder
+#                 ConditionalResidualBlock1D(
+#                     dim_in, dim_out, cond_dim=cond_dim, 
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type),
+#                 # up encoder
+#                 ConditionalResidualBlock1D(
+#                     dim_in, dim_out, cond_dim=cond_dim, 
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type)
+#             ])
+
+#         mid_dim = all_dims[-1]
+#         self.mid_modules = nn.ModuleList([
+#             ConditionalResidualBlock1D(
+#                 mid_dim, mid_dim, cond_dim=cond_dim,
+#                 kernel_size=kernel_size, n_groups=n_groups,
+#                 condition_type=condition_type
+#             ),
+#             ConditionalResidualBlock1D(
+#                 mid_dim, mid_dim, cond_dim=cond_dim,
+#                 kernel_size=kernel_size, n_groups=n_groups,
+#                 condition_type=condition_type
+#             ),
+#         ])
+
+#         down_modules = nn.ModuleList([])
+#         for ind, (dim_in, dim_out) in enumerate(in_out):
+#             is_last = ind >= (len(in_out) - 1)
+#             down_modules.append(nn.ModuleList([
+#                 ConditionalResidualBlock1D(
+#                     dim_in, dim_out, cond_dim=cond_dim, 
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type),
+#                 ConditionalResidualBlock1D(
+#                     dim_out, dim_out, cond_dim=cond_dim, 
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type),
+#                 Downsample1d(dim_out) if not is_last else nn.Identity()
+#             ]))
+
+#         up_modules = nn.ModuleList([])
+#         for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+#             is_last = ind >= (len(in_out) - 1)
+#             up_modules.append(nn.ModuleList([
+#                 ConditionalResidualBlock1D(
+#                     dim_out*2, dim_in, cond_dim=cond_dim,
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type),
+#                 ConditionalResidualBlock1D(
+#                     dim_in, dim_in, cond_dim=cond_dim,
+#                     kernel_size=kernel_size, n_groups=n_groups,
+#                     condition_type=condition_type),
+#                 Upsample1d(dim_in) if not is_last else nn.Identity()
+#             ]))
+        
+#         final_conv = nn.Sequential(
+#             Conv1dBlock(start_dim, start_dim, kernel_size=kernel_size),
+#             nn.Conv1d(start_dim, input_dim, 1),
+#         )
+        
+
+#         self.diffusion_step_encoder = diffusion_step_encoder
+#         self.local_cond_encoder = local_cond_encoder
+#         self.up_modules = up_modules
+#         self.down_modules = down_modules
+#         self.final_conv = final_conv
+#         # self.gate_score = nn.Parameter(torch.zeros(1, 2048, 4))
+
+#         logger.info(
+#             "number of parameters: %e", sum(p.numel() for p in self.parameters())
+#         )
+
+#     def forward(self, 
+#             sample: torch.Tensor, 
+#             timestep: Union[torch.Tensor, float, int], 
+#             local_cond=None, global_cond=None, **kwargs):
+#         """
+#         x: (B,T,input_dim)
+#         timestep: (B,) or int, diffusion step
+#         local_cond: (B,T,local_cond_dim)
+#         global_cond: (B,global_cond_dim)
+#         output: (B,T,input_dim)
+#         """
+#         sample = einops.rearrange(sample, 'b h t -> b t h')
+
+#         # 1. time
+#         timesteps = timestep
+#         if not torch.is_tensor(timesteps):
+#             # TODO: this requires sync between CPU and GPU. So try to pass timesteps as tensors if you can
+#             timesteps = torch.tensor([timesteps], dtype=torch.long, device=sample.device)
+#         elif torch.is_tensor(timesteps) and len(timesteps.shape) == 0:
+#             timesteps = timesteps[None].to(sample.device)
+#         # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
+#         timesteps = timesteps.expand(sample.shape[0])
+
+#         timestep_embed = self.diffusion_step_encoder(timesteps)
+#         if global_cond is not None:
+#             if self.condition_type == 'cross_attention':
+#                 timestep_embed = timestep_embed.unsqueeze(1).expand(-1, global_cond.shape[1], -1)
+#             global_feature = torch.cat([timestep_embed, global_cond], axis=-1)
+
+
+#         # encode local features
+#         h_local = list()
+#         if local_cond is not None:
+#             local_cond = einops.rearrange(local_cond, 'b h t -> b t h')
+#             resnet, resnet2 = self.local_cond_encoder
+#             x = resnet(local_cond, global_feature)
+#             h_local.append(x)
+#             x = resnet2(local_cond, global_feature)
+#             h_local.append(x)
+        
+#         x = sample
+#         h = []
+#         for idx, (resnet, resnet2, downsample) in enumerate(self.down_modules):
+#             if self.use_down_condition:
+#                 x = resnet(x, global_feature)
+#                 if idx == 0 and len(h_local) > 0:
+#                     x = x + h_local[0]
+#                 x = resnet2(x, global_feature)
+#             else:
+#                 x = resnet(x)
+#                 if idx == 0 and len(h_local) > 0:
+#                     x = x + h_local[0]
+#                 x = resnet2(x)
+#             h.append(x)
+#             x = downsample(x)
+
+#         for mid_module in self.mid_modules:
+#             if self.use_mid_condition:
+#                 x = mid_module(x, global_feature)
+#             else:
+#                 x = mid_module(x)
+
+#         for idx, (resnet, resnet2, upsample) in enumerate(self.up_modules):
+#             x = torch.cat((x, h.pop()), dim=1)
+#             if self.use_up_condition:
+#                 x = resnet(x, global_feature)
+#                 if idx == len(self.up_modules) and len(h_local) > 0:
+#                     x = x + h_local[1]
+#                 x = resnet2(x, global_feature)
+#             else:
+#                 x = resnet(x)
+#                 if idx == len(self.up_modules) and len(h_local) > 0:
+#                     x = x + h_local[1]
+#                 x = resnet2(x)
+#             x = upsample(x)
+
+
+#         x = self.final_conv(x)
+
+#         x = einops.rearrange(x, 'b t h -> b h t')
+
+#         return x
+
+
+
 class ConditionalUnet1D(nn.Module):
     def __init__(self, 
         input_dim,
         local_cond_dim=None,
         global_cond_dim=None,
         diffusion_step_embed_dim=256,
+        index_embed_dim=256,
         down_dims=[256,512,1024],
         kernel_size=3,
         n_groups=8,
@@ -177,7 +378,8 @@ class ConditionalUnet1D(nn.Module):
             nn.Mish(),
             nn.Linear(dsed * 4, dsed),
         )
-        cond_dim = dsed
+        index_encoder = nn.Linear(1, index_embed_dim)
+        cond_dim = dsed + index_embed_dim
         if global_cond_dim is not None:
             cond_dim += global_cond_dim
 
@@ -251,11 +453,11 @@ class ConditionalUnet1D(nn.Module):
         
 
         self.diffusion_step_encoder = diffusion_step_encoder
+        self.index_encoder = index_encoder
         self.local_cond_encoder = local_cond_encoder
         self.up_modules = up_modules
         self.down_modules = down_modules
         self.final_conv = final_conv
-        # self.gate_score = nn.Parameter(torch.zeros(1, 2048, 4))
 
         logger.info(
             "number of parameters: %e", sum(p.numel() for p in self.parameters())
@@ -263,7 +465,8 @@ class ConditionalUnet1D(nn.Module):
 
     def forward(self, 
             sample: torch.Tensor, 
-            timestep: Union[torch.Tensor, float, int], 
+            timestep: Union[torch.Tensor, float, int],
+            index: Union[torch.Tensor, float, int], 
             local_cond=None, global_cond=None, **kwargs):
         """
         x: (B,T,input_dim)
@@ -283,13 +486,22 @@ class ConditionalUnet1D(nn.Module):
             timesteps = timesteps[None].to(sample.device)
         # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
         timesteps = timesteps.expand(sample.shape[0])
-
         timestep_embed = self.diffusion_step_encoder(timesteps)
+        # 2. index
+        indices = index
+        if not torch.is_tensor(indices):
+            # TODO: this requires sync between CPU and GPU. So try to pass timesteps as tensors if you can
+            indices = torch.tensor([indices], dtype=torch.long, device=sample.device)
+        elif torch.is_tensor(indices) and len(indices.shape) == 0:
+            indices = indices[None].to(sample.device)
+        # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
+        indices = indices.expand(sample.shape[0])[:, None]
+        index_embed = self.index_encoder(indices)
+
         if global_cond is not None:
             if self.condition_type == 'cross_attention':
                 timestep_embed = timestep_embed.unsqueeze(1).expand(-1, global_cond.shape[1], -1)
-            global_feature = torch.cat([timestep_embed, global_cond], axis=-1)
-
+            global_feature = torch.cat([timestep_embed, index_embed, global_cond], axis=-1)
 
         # encode local features
         h_local = list()
