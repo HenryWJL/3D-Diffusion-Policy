60e3208de629b405ca7e7b16e056a13ac12bf9e0
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-01-28 15:33:04 -0500

    Update eval.sh

--- 3D-Diffusion-Policy/eval.sh
+++ 3D-Diffusion-Policy/eval.sh
@@ -3,6 +3,6 @@ task=${2}
 
 # python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/epoch%3D200_seed%3D0.pth
 # python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/epoch%3D400_seed%3D0.pth
-python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/epoch%3D600_seed%3D2.pth
-python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/epoch%3D1200_seed%3D2.pth
-python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="cpu" ckpt_paths=checkpoints/epoch%3D1800_seed%3D2.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="mps" ckpt_paths=checkpoints/epoch%3D600_seed%3D2.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="mps" ckpt_paths=checkpoints/epoch%3D1200_seed%3D2.pth
+python 3D-Diffusion-Policy/eval.py --config-name=${policy}.yaml task=${task} training.device="mps" ckpt_paths=checkpoints/epoch%3D1800_seed%3D2.pth
