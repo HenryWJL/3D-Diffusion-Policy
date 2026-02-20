1bca885920f3a80e47a264f1252b4d1e1673bef9
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-02-20 09:20:24 -0500

    Update train.sh

--- 3D-Diffusion-Policy/train.sh
+++ 3D-Diffusion-Policy/train.sh
@@ -1,10 +1,10 @@
 torchrun --nproc_per_node=2 train.py --config-name=Freqpolicy.yaml \
-                            task=robosuite_stack \
-                            task.dataset.zarr_path=/kaggle/working/3D-Diffusion-Policy/3D-Diffusion-Policy/data/robosuite_stack.zarr \
+                            task=robosuite_can \
+                            task.dataset.zarr_path=/kaggle/working/3D-Diffusion-Policy/3D-Diffusion-Policy/data/robosuite_can.zarr \
                             hf_repo_id="HenryWJL/Freqpolicy" \
-                            training.seed=2 \
+                            training.seed=0 \
                             training.num_epochs=3000 \
-                            dataloader.batch_size=224 \
+                            dataloader.batch_size=700 \
                             dataloader.num_workers=4 \
                             val_dataloader.num_workers=4 \
                             training.device="cuda" \
@@ -19,7 +19,7 @@ torchrun --nproc_per_node=2 train.py --config-name=Freqpolicy.yaml \
                             task=robosuite_square \
                             task.dataset.zarr_path=/kaggle/working/3D-Diffusion-Policy/3D-Diffusion-Policy/data/robosuite_square.zarr \
                             hf_repo_id="HenryWJL/Freqpolicy" \
-                            training.seed=0 \
+                            training.seed=2 \
                             training.num_epochs=3000 \
                             dataloader.batch_size=512 \
                             dataloader.num_workers=4 \
