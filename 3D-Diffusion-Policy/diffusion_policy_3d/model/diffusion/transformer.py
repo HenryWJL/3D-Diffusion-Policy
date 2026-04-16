61faa9e38ee2955c1a7c334387eeaf4d9b991b72
王俊麟 <wangjl@ist2-02318.apn.wlan.private.upenn.edu>
2026-04-16 16:23:04 -0400

    Add DualNoisePredictionNet

--- 3D-Diffusion-Policy/diffusion_policy_3d/model/diffusion/transformer.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/model/diffusion/transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from copy import deepcopy
 from diffusion_policy_3d.model.common.adaln_attention import AdaLNAttentionBlock, AdaLNFinalLayer
 from diffusion_policy_3d.model.common.utils import SinusoidalPosEmb, init_weights
 
@@ -99,3 +100,129 @@ class TransformerNoisePredictionNet(nn.Module):
         # Decode output
         out = self.output_decoder(x)
         return out
+
+
+class DualTimestepEncoder(nn.Module):
+    def __init__(self, embed_dim: int = 512, mlp_ratio: float = 4.0):
+        super().__init__()
+        self.sinusoidal_pos_emb = SinusoidalPosEmb(embed_dim)
+        hidden_dim = int(embed_dim * mlp_ratio)
+        self.proj = nn.Sequential(
+            nn.Linear(embed_dim * 2, hidden_dim),
+            nn.Mish(),
+            nn.Linear(hidden_dim, embed_dim),
+        )
+
+    def forward(self, t1, t2):
+        temb1 = self.sinusoidal_pos_emb(t1)
+        temb2 = self.sinusoidal_pos_emb(t2)
+        temb = torch.cat([temb1, temb2], dim=-1)
+        return self.proj(temb)
+
+
+class DualNoisePredictionNet(nn.Module):
+    def __init__(
+        self,
+        global_cond_dim: int,
+        horizon: int,
+        action_dim: int,
+        embed_dim: int = 768,
+        timestep_embed_dim: int = 512,
+        depth: int = 12,
+        num_heads: int = 12,
+        mlp_ratio: float = 4.0,
+        qkv_bias: bool = True
+    ):
+        super().__init__()
+
+        hidden_dim = int(max(action_dim, embed_dim) * mlp_ratio)
+        # Low-frequency encoder and decoder
+        self.low_freq_encoder = nn.Sequential(
+            nn.Linear(action_dim, hidden_dim),
+            nn.Mish(),
+            nn.Linear(hidden_dim, embed_dim),
+        )
+        self.low_freq_decoder = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.Mish(),
+            nn.Linear(hidden_dim, action_dim),
+        )
+        # High-frequency encoder and decoder
+        self.high_freq_encoder = deepcopy(self.low_freq_encoder)
+        self.high_freq_decoder = deepcopy(self.low_freq_decoder)
+
+        # Timestep embedding
+        self.timestep_embedding = DualTimestepEncoder(timestep_embed_dim)
+
+        # Positional embedding
+        self.pos_embed = nn.Parameter(
+            torch.empty(1, horizon, embed_dim).normal_(std=0.02)
+        )
+
+        # DiT blocks
+        cond_dim = global_cond_dim + timestep_embed_dim
+        self.blocks = nn.ModuleList(
+            [
+                AdaLNAttentionBlock(
+                    dim=embed_dim,
+                    cond_dim=cond_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                )
+                for _ in range(depth)
+            ]
+        )
+        self.head = AdaLNFinalLayer(dim=embed_dim, cond_dim=cond_dim)
+        self.horizon = horizon
+
+        # AdaLN-specific weight initialization
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Base initialization
+        self.apply(init_weights)
+
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+
+        # Zero-out output layers:
+        nn.init.constant_(self.head.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.head.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.head.linear.weight, 0)
+        nn.init.constant_(self.head.linear.bias, 0)
+
+    def forward(self, global_cond, low_freq, low_freq_t, high_freq, high_freq_t):
+        # Encode inputs
+        low_freq_embed = self.low_freq_encoder(low_freq)
+        high_freq_embed = self.high_freq_encoder(high_freq)
+
+        # Expand and encode timesteps
+        if len(low_freq_t.shape) == 0:
+            low_freq_t = low_freq_t.expand(low_freq.shape[0]).to(
+                dtype=torch.long, device=low_freq.device
+            )
+        if len(high_freq_t.shape) == 0:
+            high_freq_t = high_freq_t.expand(high_freq.shape[0]).to(
+                dtype=torch.long, device=high_freq.device
+            )
+        temb = self.timestep_embedding(low_freq_t, high_freq_t)
+
+        # Forward through model
+        x = torch.cat((low_freq_embed, high_freq_embed), dim=1)
+        x = x + self.pos_embed
+        cond = torch.cat((global_cond, temb), dim=-1)
+        for block in self.blocks:
+            x = block(x, cond)
+        x = self.head(x, cond)
+
+        # Extract low- and high-frequency predictions
+        low_freq_pred = x[:, :self.horizon/2]
+        high_freq_pred = x[:, self.horizon/2:]
+
+        # Decode outputs
+        low_freq_pred = self.low_freq_decoder(low_freq_pred)
+        high_freq_pred = self.high_freq_decoder(high_freq_pred)
+        return low_freq_pred, high_freq_pred
